610ca4e8a2d57f90fdc6da9710af0ab7bb933a67
Aron Hegyi <aron.hegyi@gusto.com>
2017-05-06 15:40:45 -0700

    Support location for person and new staff CSV

--- lib/lunch_roulette/person.rb
+++ lib/lunch_roulette/person.rb
@@ -1,22 +1,37 @@
 class LunchRoulette
   class Person
-    attr_accessor :name, :lunchable, :previous_lunches, :features, :team, :specialty, :user_id, :start_date, :email
+    attr_accessor :name,
+                  :lunchable,
+                  :previous_lunches,
+                  :features,
+                  :team,
+                  :specialty,
+                  :user_id,
+                  :start_date,
+                  :email,
+                  :location
+
     def initialize(hash)
-      @features = {}
       @lunchable = %w(true TRUE).include? hash['lunchable']
       @team = hash['team']
       @user_id = hash['user_id']
+      @name = hash['name']
       @email = hash['email']
       @specialty = hash['specialty']
       @start_date = hash['start_date']
+      @location = hash['location']
+
+      @features = {}
       @features['days_here'] = (Date.today - Date.strptime(@start_date, '%m/%d/%Y')).to_i
       @features['team'] = config.team_mappings[@team].to_i
+
       # only use specialty if mappings are given
       if config.specialty_mappings
         @features['specialty'] = config.specialty_mappings[@specialty].to_i
       end
-      @name = hash['name']
+
       @previous_lunches = []
+
       if hash['previous_lunches']
         @previous_lunches = hash['previous_lunches'].split(',').map{|i| i.to_i }
         config.maxes['lunch_id'] = @previous_lunches.max if @previous_lunches && (@previous_lunches.max > config.maxes['lunch_id'].to_i)
@@ -29,18 +44,11 @@ class LunchRoulette
     end
 
     def inspect
-      s = @name
-      if @specialty
-        s += " (#{@team} - #{@specialty})"
-      else
-        s += " (#{@team})"
-      end
-      s
+      "#{name} (#{team}#{location ? " - #{location}" : ""}#{specialty ? " - #{specialty}" : ""})"
     end
 
     def config
       LunchRoulette::Config
     end
-
   end
 end
